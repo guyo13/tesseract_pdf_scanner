305c76e911b4aae371e868728b32d28e666dc1db
Guy Or <guydude1@gmail.com>
2023-12-26 19:00:17 +0200

    fix script

--- download_and_run.sh
+++ download_and_run.sh
@@ -3,11 +3,12 @@
 BUCKET=$1
 KEY=$2
 
-if [ $BUCKET -eq "" ] then
+if [ "$BUCKET" = "" ]; then
     echo "Missing bucket name"
-elif [ $KEY -eq "" ] then
+elif [ "$KEY" = "" ]; then
     echo "Missing key"
+else
+    aws s3api get-object --bucket "$BUCKET" --key "$KEY" "$KEY" \
+    && ./search_pdf "$KEY" codes.txt all `nproc`
 fi
 
-aws s3api get-object --bucket $BUCKET --key $KEY $KEY \
-&& ./search_pdf $KEY codes.txt all `nproc`
